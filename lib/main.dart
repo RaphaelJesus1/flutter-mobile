b11fd04e2a7046a151749e73100e6efabcfa8420
Raphael Menezes de Jesus <61888147+RaphaelJesus1@users.noreply.github.com>
2022-05-21 16:25:11 -0300

    adding image on splashscreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,31 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:splashscreen/splashscreen.dart';
 import 'package:guess_the_colors_flutter/form.dart';
 
 void main() {
   runApp(GuessTheColors());
-  // WidgetsFlutterBinding.ensureInitialized();
 }
 
 class GuessTheColors extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: GameForm()
-    //     home: SplashScreen(
-    //   seconds: 5,
-    //   title: const Text(
-    //     "Guess the colors",
-    //     style: TextStyle(
-    //         color: Colors.teal, fontWeight: FontWeight.bold, fontSize: 30),
-    //   ),
-    //   navigateAfterSeconds: GameForm(),
-    //   // image: ,
-    //   // photoSize: ,
-    //   backgroundColor: Colors.amber.shade100,
-    // )
+      home: SplashScreen(
+        seconds: 5,
+        title: const Text(
+          "Guess the colors",
+          style: TextStyle(
+            color: Colors.indigo,
+            fontWeight: FontWeight.bold,
+            fontSize: 30
+          ),
+        ),
+        navigateAfterSeconds: GameForm(),
+        image: const Image(image: AssetImage('assets/logo.png')),
+        photoSize: 100,
+        backgroundColor: Colors.white,
+      )
     );
   }
 }
